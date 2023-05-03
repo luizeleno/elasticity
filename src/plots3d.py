1aeaaf1c387a7f5beb8741f81164f569fa0aa27f
SamuelMalaga <samuelmalaga99@gmail.com>
2023-05-03 13:52:27 +0000

    feat: start of 3d plot engine implementation

---
+++ src/plots3d.py
@@ -0,0 +1,54 @@
+from mayavi import mlab
+import numpy as np
+
+
+def mayaviplot(property, N=151, m=.1, d=.7, vmin=0, vmax=1, labels=['x (GPa)', 'y (GPa)', 'z (GPa)'], offscreen=True, filename='Young3D.png', *args, **kwargs):
+    '''
+        m : extent of axes
+        d: POV distance
+    '''
+
+    N = int(N) * 1j
+    theta, phi = np.mgrid[0:np.pi:N, 0:2*np.pi:N]
+    ldir = np.array([np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)])
+    case = property(ldir)
+
+    print(case.min(), case.max())
+
+    x = case * ldir[0]
+    y = case * ldir[1]
+    z = case * ldir[2]
+
+    if offscreen:
+        mlab.options.offscreen = True
+
+    mlab.figure(1, bgcolor=(1, 1, 1), fgcolor=(0, 0, 0), size=(1050, 1000))
+    mlab.clf()
+
+    mlab.mesh(x, y, z, scalars=case, colormap='plasma')
+
+    mlab.mesh(x, y, z, representation='wireframe', color=(.2, .2, .2), opacity=.1)
+
+    extent = [-m, m, -m, m, -m, m]
+    ax = mlab.axes(extent=extent, xlabel=labels[0], ylabel=labels[1], zlabel=labels[2])
+    ax.axes.font_factor = 2
+    mlab.outline(extent=extent)
+
+    mlab.view(-120, 60, d, [0, 0, 0])
+
+    if offscreen:
+        mlab.savefig(filename)
+    else:
+        mlab.show()
+
+def YoungModulus3DPlot(material, *args, **kwargs):
+
+    E = material.YoungModulus
+
+    mayaviplot(E, *args, **kwargs)
+
+def LinearCompressibility3DPlot(material, *args, **kwargs):
+
+    B = material.LinearCompressibility
+
+    mayaviplot(B, *args, **kwargs)
