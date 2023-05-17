5fe7cb7f0030bc955ebe64403e5227afdac4daac
SamuelMalaga <samuelmalaga99@gmail.com>
2023-05-17 15:49:30 +0000

    build: added imports to init file for better usability

--- src/elasticityproject/__init__.py
+++ src/elasticityproject/__init__.py
@@ -1,3 +1,5 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("elasticityproject")
+from elasticityproject.elasticityproject import ElasticityTheory,DirectionalProperties,VRH
+from elasticityproject.plots3d import mayaviplot,LinearCompressibility3DPlot,YoungModulus3DPlot
